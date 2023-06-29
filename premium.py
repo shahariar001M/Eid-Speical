fc3ec719aa78b06b3e43b3775603bf8d303e9a45
Shahariar Zaman <126738052+shahariar001M@users.noreply.github.com>
2023-06-29 10:48:34 +0600

    Update premium.py

--- premium.py
+++ premium.py
@@ -1,6 +1,6 @@
-# Encrypted by: Tutul King 
-# Whatsapp: +8801608843956
-# Github: https://github.com/Tutul-King
+# Encrypted by: Cyber Mafia Innocent King
+# Telegram :https://t.me/InnocentDR4G0NHACKER
+# Github: https://github.com/shahariar001M
 
 exec("".join(map(chr,[int("".join(str({'😀': 0, '😁': 3, '😂': 6, '😃': 1, '😄': 2, '😅': 4, '😉': 7, '😊': 8, '😛': 9, '🤣': 5}[i]) for i in x.split())) for x in
 "😃 😀 😄  😃 😃 😅  😃 😃 😃  😃 😀 😛  😁 😄  😛 😊  😃 😃 🤣  🤣 😄  😁 😄  😃 😀 🤣  😃 😀 😛  😃\
